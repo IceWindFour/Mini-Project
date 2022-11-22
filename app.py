0807e2e59bb1541833fa6184dbc4b4ab6d35347c
icewindfour <icewindfour@gmail.com>
2022-11-22 22:33:41 +0000

    week 5/6

--- app.py
+++ app.py
@@ -1,77 +1,83 @@
-import functions as pf
-from functions import Menu
+import src.functions as pf
+from src.functions import Menu
+import data.database as database
+from src.dbfunctions import *
 
-pf.read_file()
-main_menu_input = Menu().main_menu()
+# pf.read_file()
+# main_menu_input = Menu()
 
-while True:
-    if main_menu_input == "1":
-        products_menu_input = Menu().products_menu()
+def uplink():
+    connection = database.connect()
+    database.create_tables(connection)
 
-        if products_menu_input == "0":
-            print("Returning to Main Menu")
+    main_menu_input = Menu()
+
+    while True:
+        if main_menu_input == "1":
+            products_menu_input = Menu().products_menu()
+
+            if products_menu_input == "0":
+                print("Returning to Main Menu")
+                main_menu_input = Menu().main_menu()
+
+            elif products_menu_input == "1":
+                display_products(connection)
+
+            elif products_menu_input == "2":
+                add_new_product(connection)
+
+            elif products_menu_input == "3":
+                update_product(connection)
+
+            elif products_menu_input == "4":
+                delete_product(connection)
+
+        elif main_menu_input == "2":
+            order_menu_input = Menu().order_menu()
+
+            if order_menu_input == "0":
+                print("Returning to Main Menu")
+                main_menu_input = Menu().main_menu()
+            
+            elif order_menu_input == "1":
+                display_orders(connection)
+                    
+            elif order_menu_input == "2":
+                add_new_order(connection)
+
+            elif order_menu_input == "3":
+                update_order_status(connection)
+
+            elif order_menu_input == "4":
+                update_order(connection)
+
+            elif order_menu_input == "5":
+                delete_order(connection)
+
+        elif main_menu_input == "3":
+            courier_menu_input = Menu().courier_menu()
+
+            if courier_menu_input == "0":
+                print("Returning to Main Menu")
+                main_menu_input = Menu().main_menu()
+
+            elif courier_menu_input == "1":
+                display_couriers(connection)
+
+            elif courier_menu_input == "2":
+                add_new_courier(connection)
+            
+            elif courier_menu_input == "3":
+                update_courier(connection)
+            
+            elif courier_menu_input == "4":
+                delete_courier(connection)
+
+        elif main_menu_input == "0":
+            print("bye")
+            exit()
+        else:
+            print("Not a vaild input")
             main_menu_input = Menu().main_menu()
-
-        elif products_menu_input == "1":
-            pf.product_print()
-
-        elif products_menu_input == "2":
-            pf.new_product()
-
-        elif products_menu_input == "3":
-            pf.updating_product()
-
-        elif products_menu_input == "4":
-            pf.delete_product()
-
-    elif main_menu_input == "2":
-        order_menu_input = Menu().order_menu()
-
-        if order_menu_input == "0":
-            print("Returning to Main Menu")
-            main_menu_input = Menu().main_menu()
-
-        elif order_menu_input == "1":
-            pf.order_print()
-
-        elif order_menu_input == "2":
-            pf.customer_inputs()
-            # still need to improve the function
-
-        elif order_menu_input == "3":
-            pf.update_status()
-
-        elif order_menu_input == "4":
-            pf.update_order()
-            # doesn't meet the spec yet
-
-        elif order_menu_input == "5":
-            pf.delete_order()
-
-    elif main_menu_input == "3":
-        courier_menu_input = Menu().courier_menu()
-
-        if courier_menu_input == "0":
-            print("Returning to Main Menu")
-            main_menu_input = Menu().main_menu()
-
-        elif courier_menu_input == "1":
-            pf.courier_print()
-
-        elif courier_menu_input == "2":
-            pf.new_courier()
-
-        elif courier_menu_input == "3":
-            pf.updating_courier()
-
-        elif courier_menu_input == "4":
-            pf.delete_courier()
-
-    elif main_menu_input == "0":
-        pf.write_file()
-        print("bye")
-        exit()
-
-    else:
-        print("Not a vaild input")
-        main_menu_input = Menu().main_menu()
+            
+uplink()
